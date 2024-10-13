028bf0bb72362e1344d94d551cba6c8950dcf68b
Rayan Imran <rayanimran307@gmail.com>
2024-10-13 15:05:19 -0500

    background image added

--- frontend/src/components/Blog/BlogCard.jsx
+++ frontend/src/components/Blog/BlogCard.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const BlogCard = ({ id, title, description }) => {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg m-4">
+    <div className="transform hover:scale-105 transition-transform duration-300 bg-white border border-gray-200 rounded-lg shadow-lg m-4 overflow-hidden hover:shadow-2xl">
       <div className="p-5">
-        <h2 className="text-2xl font-bold mb-2">{title}</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">{title}</h2>
         <p className="mb-3 text-gray-700">{description}</p>
         <Link
           to={`/blog/${id}`}
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition-colors"
         >
           Read More
         </Link>
